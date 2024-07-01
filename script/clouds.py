b43652cfe3d6f697eab2d5b029fcc6a36d9c8c68
tongthanhdat009 <gamingthanhdat@gmail.com>
2024-07-01 20:18:31 +0700

    a
    a

---
+++ script/clouds.py
@@ -0,0 +1,33 @@
+import random
+import pygame
+
+class cloud:
+    def __init__(self,pos,img,speed,depth):
+        self.pos = list(pos)
+        self.img = img
+        self.speed = speed
+        self.depth = depth
+
+    def update(self):
+        self.pos[0] += self.speed
+    
+    def render(self, surf, offset=(0,0)):
+        render_pos=(self.pos[0] - offset[0] * self.depth, self.pos[1] - offset[1]*self.depth)
+        surf.blit(self.img, (render_pos[0] % (surf.get_width() + self.img.get_width()) -self.img.get_width(),render_pos[1] % (surf.get_height() + self.img.get_height())-self.img.get_height()))
+class clouds:
+    def __init__(self, cloud_images, count=16):
+        self.clouds =[]
+        
+        #chọn xuất ngẫu nhiên mây
+        for i in range(count):
+            self.clouds.append(cloud((random.random()*99999,random.random()*99999),random.choice(cloud_images), random.random()*0.05+0.05, random.random()*0.6+0.2))
+
+        self.clouds.sort(key=lambda x: x.depth)
+    
+    def update(self):
+        for cloud in self.clouds:
+            cloud.update()
+    
+    def render(self, surf, offset=(0,0)):
+        for cloud in self.clouds:
+            cloud.render(surf, offset=offset)
---
+++ test.py
@@ -0,0 +1,112 @@
+import sys
+import pygame
+
+
+from script.entities import PhysicsEntity, Player
+from script.utils import load_image, load_images, animation
+from script.tilemap import Tilemap
+from script.clouds import clouds
+class test:
+    def __init__(self):#khởi tạo cửa sỏ    
+        pygame.init()
+
+        pygame.display.set_caption("test") # đổi tiêu đề cửa sổ
+        self.screen = pygame.display.set_mode((640,480))
+        self.display = pygame.Surface((320,240))
+
+        self.clock = pygame.time.Clock()
+
+        #ảnh bầu trời
+        try:
+            self.bg = pygame.image.load('data//images//background.png')
+            self.bg = pygame.transform.scale(self.bg, (640,480))  # Resize the image
+        except FileNotFoundError:
+            print("Background image file not found.")
+            self.bg = None
+
+        self.movement = [False,False,False,False]
+
+        #hình ảnh
+        self.assets = {
+            'decor': load_images('tiles//decor'),
+            'grass': load_images('tiles//grass'),
+            'large_decor': load_images('tiles//large_decor'),
+            'stone': load_images('tiles//stone'),
+            'player': load_image('entities//player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
+            'player//idle': animation(load_images('entities//player//idle'),img_dur=6),
+            'player//run': animation(load_images('entities//player//run'),img_dur=4),
+            'player//jump': animation(load_images('entities//player//jump')),
+            'player//slide': animation(load_images('entities//player//slide')),
+            'player//wall_slide': animation(load_images('entities//player//wall_slide')),
+        }
+        print(self.assets)
+
+        #đối tượng mây
+        self.clouds = clouds(self.assets['clouds'],count=10)
+
+        #đối tượng người chơi
+        self.player = Player(self, (50,50), (8,15))
+
+        #đối tượng vật thể trong map
+        self.tilemap = Tilemap(self,tile_size=16)
+
+        #điều chỉnh camera
+        self.scroll = [0,0]
+
+    def run(self):#chạy game
+        run = True
+
+        while run:
+            self.display.blit(self.assets['background'],(0,0))
+            
+            #cho cam tự di chuyển
+            # self.scroll[0]+=1
+            self.scroll[0] += (self.player.rect().centerx-self.display.get_width()/2-self.scroll[0])/30
+            self.scroll[1] += (self.player.rect().centery-self.display.get_width()/2-self.scroll[1])/30
+            render_scroll = (int(self.scroll[0]),int(self.scroll[1]))
+            
+            #tạo vật thể trong map
+            self.tilemap.render(self.display,offset=render_scroll)            
+
+            self.clouds.update()
+            self.clouds.render(self.display,offset=render_scroll)
+
+            #spawn
+            self.player.update(self.tilemap,(self.movement[1]-self.movement[0],0))
+            self.player.render(self.display,offset=render_scroll)
+
+
+            for event in pygame.event.get(): #sự kiện tương tác trong game
+                #cài đặt nút di chuyển 
+                #di chuyển bằng WASD hoặc mũi tên
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP or event.key==pygame.K_w:
+                        self.player.velocity[1] = -3
+                    #     self.movement[0] = True
+                    # if event.key == pygame.K_DOWN or event.key==pygame.K_s:
+                    #     self.movement[1] = True
+                    if event.key == pygame.K_LEFT or event.key==pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT or event.key==pygame.K_d:
+                        self.movement[1] = True
+
+                if event.type == pygame.KEYUP:
+                    # if event.key == pygame.K_DOWN or event.key==pygame.K_s:
+                    #     self.movement[1] = False
+                    if event.key == pygame.K_LEFT or event.key==pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT or event.key==pygame.K_d:
+                        self.movement[1] = False
+                        
+                            
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.screen.blit(pygame.transform.scale(self.display,self.screen.get_size()),(0,0))
+            pygame.display.update()
+            self.clock.tick(60) #cài đặt 60fps
+
+test().run()
